cd8e9a72ca2dc54eddfdc7f3bca29fd7ead5dad6
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2025-11-01 13:53:57 -0300

    feat:(frontend) finish footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className='bg-blue-900 h-[88px] flex items-center '>
+    <div className='bg-blue-900 h-[220px] md:h-[88px] flex flex-col md:flex-row md:justify-between items-center gap-1 '>
       <div className='mx-4 md:mx-[88px]'>
         <img
           className='mt-4 mb-4 md:mb-6 md:mt-6'
           src='/src/assets/logo2.svg'
           alt='logo'
         />
+      </div>
+      <div className='mx-4 md:mx-[88px] flex flex-col md:flex-row gap-4'>
+        <img src='/src/assets/Telefone.svg' alt='telefone' />
+        <img src='/src/assets/Zap.svg' alt='zap' />
       </div>
     </div>
   );
